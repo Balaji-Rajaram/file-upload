546f9a9e3a4f0ddc8a84db868684b3758a91936e
balajilaky <51748359+balajilaky@users.noreply.github.com>
2020-05-31 13:52:30 +0530

    first

---
+++ src/routes/web.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const uploadController = require("../controllers/upload");
+
+let routes = app => { 
+  router.post("/upload", uploadController.uploadFile);
+
+  return app.use("/", router);
+};
+
+module.exports = routes;
